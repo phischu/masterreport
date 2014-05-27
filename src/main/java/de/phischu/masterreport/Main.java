5dfb9e679474cdd988ab52bbd8058503cbdb1c54
phischu <pschuster@uni-koblenz.de>
2014-05-27 17:01:55 +0200

    empty chart

--- src/main/java/de/phischu/masterreport/Main.java
+++ src/main/java/de/phischu/masterreport/Main.java
@@ -1,8 +1,29 @@
 package de.phischu.masterreport;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.DefaultXYItemRenderer;
+import org.jfree.data.xy.DefaultXYDataset;
+
 public class Main {
 	
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
+		
+		DefaultXYDataset dataset = new DefaultXYDataset();
+		NumberAxis xaxis = new NumberAxis();
+		NumberAxis yaxis = new NumberAxis();
+		DefaultXYItemRenderer itemrenderer = new DefaultXYItemRenderer();
+		
+		XYPlot plot = new XYPlot(dataset,xaxis,yaxis,itemrenderer);
+		
+		JFreeChart chart = new JFreeChart(plot);
+		
+		ChartUtilities.saveChartAsPNG(new File("chart.png"), chart, 1024, 768);
 		
 	}
 
