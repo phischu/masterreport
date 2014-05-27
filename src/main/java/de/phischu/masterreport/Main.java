e249cdf3829ba4783c85d9a89c898c337ca1ec70
phischu <pschuster@uni-koblenz.de>
2014-05-27 17:37:31 +0200

    generate pie chart

--- src/main/java/de/phischu/masterreport/Main.java
+++ src/main/java/de/phischu/masterreport/Main.java
@@ -1,29 +1,34 @@
 package de.phischu.masterreport;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.DefaultXYItemRenderer;
-import org.jfree.data.xy.DefaultXYDataset;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException{
 		
-		DefaultXYDataset dataset = new DefaultXYDataset();
-		NumberAxis xaxis = new NumberAxis();
-		NumberAxis yaxis = new NumberAxis();
-		DefaultXYItemRenderer itemrenderer = new DefaultXYItemRenderer();
+		DefaultPieDataset dataset = new DefaultPieDataset();
+		dataset.setValue("All packages", 30000);
+		dataset.setValue("Attempted packages", 2345);
+		dataset.setValue("Successful packages", 1034);
 		
-		XYPlot plot = new XYPlot(dataset,xaxis,yaxis,itemrenderer);
+		PiePlot plot = new PiePlot(dataset);
+		plot.setSectionPaint("All packages", Color.gray);
+		plot.setSectionPaint("Attempted packages", Color.red);
+		plot.setSectionPaint("Successful packages", Color.green);
+		
 		
 		JFreeChart chart = new JFreeChart(plot);
 		
 		ChartUtilities.saveChartAsPNG(new File("chart.png"), chart, 1024, 768);
+		
+		System.out.println("done");
 		
 	}
 
