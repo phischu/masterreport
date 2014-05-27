7f148ae9407cc5559bfc1cc0266e77a1835efb56
phischu <pschuster@uni-koblenz.de>
2014-05-27 17:43:27 +0200

    factor out package plotting

--- src/main/java/de/phischu/masterreport/Main.java
+++ src/main/java/de/phischu/masterreport/Main.java
@@ -13,22 +13,27 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException{
 		
+        plotPackages(30000,2355,1034);
+		
+		System.out.println("done");
+		
+	}
+	
+	public static void plotPackages(Number allpackages,Number attemptedpackages,Number successfulpackages) throws IOException{
+		
 		DefaultPieDataset dataset = new DefaultPieDataset();
-		dataset.setValue("All packages", 30000);
-		dataset.setValue("Attempted packages", 2345);
-		dataset.setValue("Successful packages", 1034);
+		dataset.setValue("All packages", allpackages);
+		dataset.setValue("Attempted packages", attemptedpackages);
+		dataset.setValue("Successful packages", successfulpackages);
 		
 		PiePlot plot = new PiePlot(dataset);
 		plot.setSectionPaint("All packages", Color.gray);
 		plot.setSectionPaint("Attempted packages", Color.red);
 		plot.setSectionPaint("Successful packages", Color.green);
 		
-		
 		JFreeChart chart = new JFreeChart(plot);
 		
 		ChartUtilities.saveChartAsPNG(new File("chart.png"), chart, 1024, 768);
-		
-		System.out.println("done");
 		
 	}
 
