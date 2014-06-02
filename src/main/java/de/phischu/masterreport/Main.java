f284a21fa630a2d430a6b501581274544792808b
phischu <pschuster@uni-koblenz.de>
2014-06-02 12:08:21 +0200

    minor refactoring

--- src/main/java/de/phischu/masterreport/Main.java
+++ src/main/java/de/phischu/masterreport/Main.java
@@ -5,10 +5,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
@@ -31,20 +33,13 @@ import org.neo4j.tooling.GlobalGraphOperations;
 public class Main {
 
 	public static final String DB_PATH = "data";
-	
-	public enum Labels implements Label
-	{
-		Package,
-		Declaration,
-		Symbol
-    }
-	
-	private static enum RelationshipTypes implements RelationshipType
-	{
-		DEPENDENCY,
-	    DECLARATION,
-	    MENTIONEDSYMBOL,
-	    DECLAREDSYMBOL
+
+	public enum Labels implements Label {
+		Package, Declaration, Symbol
+	}
+
+	private static enum RelationshipTypes implements RelationshipType {
+		DEPENDENCY, DECLARATION, MENTIONEDSYMBOL, DECLAREDSYMBOL
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -78,110 +73,130 @@ public class Main {
 		System.out.println("done");
 
 	}
-	
-	public static void plotPackages(GraphDatabaseService graphDb) throws IOException{
-		
-		Iterable<Node> packagenodes = GlobalGraphOperations.at(graphDb).getAllNodesWithLabel(Labels.Package);
+
+	public static void plotPackages(GraphDatabaseService graphDb)
+			throws IOException {
+
+		Iterable<Node> packagenodes = GlobalGraphOperations.at(graphDb)
+				.getAllNodesWithLabel(Labels.Package);
 		long attemptedpackages = 0;
 		long successfulpackages = 0;
-		for(Node packagenode : packagenodes){
+		for (Node packagenode : packagenodes) {
 			attemptedpackages += 1;
-			if(packagenode.hasRelationship(Direction.OUTGOING, RelationshipTypes.DECLARATION)){
+			if (packagenode.hasRelationship(Direction.OUTGOING,
+					RelationshipTypes.DECLARATION)) {
 				successfulpackages += 1;
 			}
 		}
-		
+
 		long allpackages = 40160;
-		
+
 		DefaultPieDataset dataset = new DefaultPieDataset();
 		dataset.setValue("All packages", allpackages - attemptedpackages);
-		dataset.setValue("Attempted packages", attemptedpackages - successfulpackages);
+		dataset.setValue("Attempted packages", attemptedpackages
+				- successfulpackages);
 		dataset.setValue("Successful packages", successfulpackages);
-		
+
 		PiePlot plot = new PiePlot(dataset);
 		plot.setSectionPaint("All packages", Color.gray);
 		plot.setSectionPaint("Attempted packages", Color.red);
 		plot.setSectionPaint("Successful packages", Color.green);
-		
+
 		JFreeChart chart = new JFreeChart(plot);
-		
+
 		ChartUtilities.saveChartAsPNG(new File("chart.png"), chart, 1024, 768);
-		
+
 	}
-	
-	public static Collection<Pair<String,String>> refactoring(GraphDatabaseService graphDb){
-		
-		Collection<Pair<String,String>> declarationastpairs = new LinkedList<Pair<String,String>>();
-		
-		Iterable<Node> symbolnodes = GlobalGraphOperations.at(graphDb).getAllNodesWithLabel(Labels.Symbol);
-		
-		for(Node symbolnode : symbolnodes){
-			
-			Iterable<Pair<Node,Node>> usages = usage(symbolnode);
-			
-			for(Pair<Node,Node> usage1 : IteratorUtil.asCollection(usages)){
-				for(Pair<Node,Node> usage2 : IteratorUtil.asCollection(usages)){
-					
-					String usingast1 = (String) usage1.first().getProperty("declarationast");
-					String usedast1 = (String) usage1.other().getProperty("declarationast");
-					String usingast2 = (String) usage2.first().getProperty("declarationast");
-					String usedast2 = (String) usage2.other().getProperty("declarationast");
-					
-					if(usingast1.equals(usingast2) && !usedast1.equals(usedast2)){
+
+	public static Collection<Pair<String, String>> refactoring(
+			GraphDatabaseService graphDb) {
+
+		Collection<Pair<String, String>> declarationastpairs = new LinkedList<Pair<String, String>>();
+
+		Iterable<Node> symbolnodes = GlobalGraphOperations.at(graphDb)
+				.getAllNodesWithLabel(Labels.Symbol);
+
+		for (Node symbolnode : symbolnodes) {
+
+			Iterable<Pair<Node, Node>> usages = usage(symbolnode);
+
+			for (Pair<Node, Node> usage1 : IteratorUtil.asCollection(usages)) {
+				for (Pair<Node, Node> usage2 : IteratorUtil
+						.asCollection(usages)) {
+
+					String usingast1 = (String) usage1.first().getProperty(
+							"declarationast");
+					String usedast1 = (String) usage1.other().getProperty(
+							"declarationast");
+					String usingast2 = (String) usage2.first().getProperty(
+							"declarationast");
+					String usedast2 = (String) usage2.other().getProperty(
+							"declarationast");
+
+					if (usingast1.equals(usingast2)
+							&& !usedast1.equals(usedast2)) {
 						declarationastpairs.add(Pair.of(usedast1, usedast2));
 					}
-					
+
 				}
 			}
-			
-			
+
 		}
-			
+
 		return declarationastpairs;
-		
+
 	}
-	
+
 	private static Iterable<Pair<Node, Node>> usage(Node symbolnode) {
-		
-		LinkedList<Pair<Node,Node>> usages = new LinkedList<Pair<Node,Node>>();
-		
-		Iterable<Node> usingnodes = previousNodes(RelationshipTypes.MENTIONEDSYMBOL, symbolnode);
-		Iterable<Node> usednodes = previousNodes(RelationshipTypes.DECLAREDSYMBOL, symbolnode);
-		
-		for(Node usingnode : usingnodes){
-			for(Node usednode : usednodes){
-				
-				for(Node dependingnode : previousNodes(RelationshipTypes.DECLARATION,usingnode)){
-					for(Node dependencynode : previousNodes(RelationshipTypes.DECLARATION, usednode)){
-						
-						Collection<Node> dependencynodes = IteratorUtil.asCollection(nextNodes(RelationshipTypes.DEPENDENCY,dependingnode));
-						
-						if(dependencynodes.contains(dependencynode)){
+
+		LinkedList<Pair<Node, Node>> usages = new LinkedList<Pair<Node, Node>>();
+
+		Iterable<Node> usingnodes = previousNodes(
+				RelationshipTypes.MENTIONEDSYMBOL, symbolnode);
+		Iterable<Node> usednodes = previousNodes(
+				RelationshipTypes.DECLAREDSYMBOL, symbolnode);
+
+		for (Node usingnode : usingnodes) {
+			for (Node usednode : usednodes) {
+
+				for (Node dependingnode : previousNodes(
+						RelationshipTypes.DECLARATION, usingnode)) {
+					for (Node dependencynode : previousNodes(
+							RelationshipTypes.DECLARATION, usednode)) {
+
+						Collection<Node> dependencynodes = IteratorUtil
+								.asCollection(nextNodes(
+										RelationshipTypes.DEPENDENCY,
+										dependingnode));
+
+						if (dependencynodes.contains(dependencynode)) {
 							usages.add(Pair.of(usingnode, usednode));
 						}
 					}
 				}
 			}
 		}
-		
+
 		return usages;
-		
+
 	}
 
-	private static Iterable<Node> nextNodes(RelationshipTypes relationshiptype,Node node) {
-		return Iterables.map(
-				new Function<Relationship,Node>(){
-					public Node apply(Relationship relationship) {
-						return relationship.getEndNode();}},
-				node.getRelationships(Direction.OUTGOING, relationshiptype));
+	private static Iterable<Node> nextNodes(RelationshipTypes relationshiptype,
+			Node node) {
+		return Iterables.map(new Function<Relationship, Node>() {
+			public Node apply(Relationship relationship) {
+				return relationship.getEndNode();
+			}
+		}, node.getRelationships(Direction.OUTGOING, relationshiptype));
 	}
 
-	public static Iterable<Node> previousNodes(RelationshipType relationshiptype,Node node){
-		return Iterables.map(
-				new Function<Relationship,Node>(){
-					public Node apply(Relationship relationship) {
-						return relationship.getStartNode();}},
-				node.getRelationships(Direction.INCOMING, relationshiptype));
+	public static Iterable<Node> previousNodes(
+			RelationshipType relationshiptype, Node node) {
+		return Iterables.map(new Function<Relationship, Node>() {
+			public Node apply(Relationship relationship) {
+				return relationship.getStartNode();
+			}
+		}, node.getRelationships(Direction.INCOMING, relationshiptype));
 	}
 
 }
