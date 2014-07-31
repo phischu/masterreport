6e430bf001291ff0ad6ded1a452e38cc16edffe7
phischu <pschuster@uni-koblenz.de>
2014-07-31 16:36:31 +0200

    update members

--- src/main/java/de/phischu/masterreport/Update.java
+++ src/main/java/de/phischu/masterreport/Update.java
@@ -1,5 +1,13 @@
 package de.phischu.masterreport;
 
 public class Update {
+	
+	public String packagename;
+	public String packageversion;
+	public String dependencyname1;
+	public String dependencyversion1;
+	public String dependencyname2;
+	public String dependencyversion2;
+	public Boolean safe;
 
 }
